5355a1a5d20cf60a05579a4e1619227d4330052b
Deepak Sharma <ce.deepaksharma@gmai.com>
2024-09-03 13:17:06 +0530

    add custom css

--- dist/server.js
+++ dist/server.js
@@ -27,7 +27,10 @@ const CSS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger
 //   './node_modules/swagger-ui-dist/swagger-ui-bundle.js',
 //   './node_modules/swagger-ui-dist/swagger-ui-standalone-preset.js'
 // ];
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, { customCssUrl: CSS_URL })
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+    customCss: '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+    customCssUrl: CSS_URL
+})
 // swaggerUi.setup(swaggerSpec, { customCssUrl: CSS_URL, customJs: JS_URL })
 );
 // till here
--- server.ts
+++ server.ts
@@ -35,7 +35,11 @@ const CSS_URL =
 app.use(
   '/api-docs',
   swaggerUi.serve,
-  swaggerUi.setup(swaggerSpec, { customCssUrl: CSS_URL })
+  swaggerUi.setup(swaggerSpec, {
+    customCss:
+      '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+    customCssUrl: CSS_URL
+  })
   // swaggerUi.setup(swaggerSpec, { customCssUrl: CSS_URL, customJs: JS_URL })
 );
 
